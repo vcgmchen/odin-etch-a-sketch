d9e5dcbce78539599690e77db166b27c9c32bf3e
vcgmchen <149526960+vcgmchen@users.noreply.github.com>
2024-07-10 21:32:37 -0400

    Set up hover effect to change square color during mouseover event

--- README.md
+++ README.md
@@ -13,3 +13,22 @@ The size of the grid can be set by the user by clicking a button that allows the
 ## thought process log: 
 * it can be useful to write up the HTML first, what the actual HTML should look like, before going and coding the JS function that's supposed to produce the HTML code automatically 
 * i don't need a reference to each indiivudal div square, so it makes sense to skip caching the reference in a variable, and perform the create element function within the append child funciton 
+
+## future work 
+* Make it a pixel painter 
+
+---
+## todo 
+phase 3 pseudocode: 
+hover over squares -> change background color 
+to do:
+- [X] read about hover events 
+- [X] read about event bubbling
+- [ ] read about removing event listeners?
+Q: should I make multiple commits for each progressive improvement on the hover step?
+- [X] Take notes on git commit philosophies
+- [ ] Plan out commit(s) for current step (step 3)  
+- [ ] see if need to put code in a function, or not / leave as global in script 
+- [X] Make commit 
+- [ ] Move on to step 4 (button)
+gi
--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// Set up page with a grid of initial size
+const INITIAL_SIZE = 16;
+createGrid(INITIAL_SIZE);
+
 // Function to create a 16x16 grid of divs 
-function createGrid() {
-    
+function createGrid(size) {
+
     // Store DOM selector for container div
     const container = document.querySelector("#container");
-    const SIZE = 16;
 
     // Create the grid rows by creating div elements 
     // and appending directly to container div
-    for (let row = 0; row < SIZE; row++) {
+    for (let row = 0; row < size; row++) {
         container.appendChild(document.createElement("div"));
     }
 
@@ -16,12 +19,27 @@ function createGrid() {
 
     // Create the grid squares by creating div elements within each row div 
     // (nested) and appending directly to row divs
-    for (let row = 0; row < SIZE; row++) {
-        for (let col = 0; col < SIZE; col++) {
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
             containerRow[row].appendChild(document.createElement("div"));
         }
     }
+
 }
 
-// Run the function
-createGrid();
+
+// Select all squares of the grid
+const squares = document.querySelectorAll("#container > div > div");
+
+// Helper function that applies CSS class to a given square 
+const handleMouseover = function () {
+    // `this` references the event's target element - i.e. the square 
+    this.classList.add("colored");
+}
+
+// Iterate over the nodeList of squares to add event listener for hover
+squares.forEach((square) => {
+    square.addEventListener("mouseover", handleMouseover);
+})
+
+
